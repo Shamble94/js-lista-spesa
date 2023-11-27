b24eb9e72f5f199964bfd6eb932e5a01151080e9
Shamble94 <fratus.nicholas@gmail.com>
2023-11-27 15:41:55 +0100

    Esercizio finito

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* DICHIARAZIONE ARRAY SPESA */
+const spesa = ["uova", "pecorino", "guanciale", "pepe", "pasta", "pane", "tonno", "insalata"]
+
+/* DICHIARAZIONE VARIABILE i A 0 */
+let i = 0;
+
+/* CICLO WHILE */ /* FINCHE i è MINORE DELLA LUNGHEZZA DELL'ARRAY */
+while( i < spesa.length){
+    
+    /* COSTANTE prodotto */
+    const prodotto = spesa[i] ;
+
+    /* ANDIAMO A RIPRENDERE LA LISTA NELL'HTML E GLI AGGIUNGIAMO UNA LI CON IL PRODOTTO AD OGNI CICLO */
+    document.querySelector(`ul`).innerHTML += `<li>${prodotto}</li>`
+
+    /* AD OGNI CICLO i AUMENTA DI 1 */
+    i++
+
+    /* STAMPIAMO I PRODOTTI IN CONSOLE */
+    console.log(prodotto)
+}
